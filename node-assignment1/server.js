1f5c533665dca2ee4a840b8fffde753959cec9a6
Dariaki <dashakirich@gmail.com>
2020-03-18 18:59:21 +0200

    replace async functions with async callback inside createServer method

--- node-assignment1/server.js
+++ node-assignment1/server.js
@@ -8,49 +8,37 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-
 const port = 3000;
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-http.createServer((request, response) => {
+http.createServer(async (request, response) => {
 
   if(request.url === '/requests') {
-      response.writeHead(200, {'Content-type': 'text/json'});
+      response.writeHead(200, {'Content-type': 'application/json'});
+      const data = await readFileAsync(path.join(__dirname, 'requests.json'), 'utf-8');
 
-    requestAll().then((result) => {
-      response.end(result);
-    })
-
+      return response.end(data);
   }
 
-  requestsManager(request).then(() => {
-    response.writeHead(200, {'Content-type': 'application/json'});
-    return response.end(JSON.stringify({status: 'OK'}));
+  const data = await readFileAsync(path.join(__dirname, 'requests.json'), 'utf-8');
+  const dataParsed = JSON.parse(data);
+  dataParsed.logs.push({
+    method: request.method,
+    url: request.url,
+    time: new Date().getTime()
+  });
 
-  })
+  try {
+    await writeFileAsync(path.join(__dirname, 'requests.json'), JSON.stringify(dataParsed, null, 4), 'utf-8');
+  } catch(e) {
+    console.log('Error happened:', e);
+  }
 
+  response.writeHead(200, {'Content-type': 'application/json'});
+  return response.end(JSON.stringify({status: 'OK'}));
 
 }).listen(port, () => {
   console.log('Server is listening on 3000...');
 });
-
-
-async function requestAll() {
-  return await readFileAsync(path.join(__dirname, 'requests.json'), 'utf-8');
-}
-
-async function requestsManager(req) {
-  const data = await readFileAsync(path.join(__dirname, 'requests.json'), 'utf-8');
-
-  const dataParsed = JSON.parse(data);
-  dataParsed.logs.push({
-    method: req.method,
-    url: req.url,
-    time: new Date().getTime()
-  });
-
-  return await writeFileAsync(path.join(__dirname, 'requests.json'), JSON.stringify(dataParsed, null, 4), 'utf-8');
-
-}
