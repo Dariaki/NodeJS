946cf3463796c07f4f713b793f194fa41a4160e4
Dariaki <dashakirich@gmail.com>
2020-03-18 17:39:05 +0200

    add readFile method, re-wrote with promisify

--- node-assignment1/server.js
+++ node-assignment1/server.js
@@ -3,39 +3,54 @@
 // Organize all information inside one JSON file;
 // Write every request info to array in json file.
 
+const util = require('util');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const requestInfo = {};
-requestInfo.logs = [];
 
-fs.writeFile(path.join(__dirname, 'requests.json'), JSON.stringify(requestInfo, null, 4), () => {
-  console.log('Json file was created!')
-});
+const port = 3000;
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 http.createServer((request, response) => {
 
   if(request.url === '/requests') {
-    response.writeHead(200, {'Content-type': 'text/json'});
-    response.end(JSON.stringify(requestInfo));
+      response.writeHead(200, {'Content-type': 'text/json'});
+
+    requestAll().then((result) => {
+      response.end(result);
+    })
+
   }
 
+  requestsManager(request).then(() => {
+    response.writeHead(200, {'Content-type': 'application/json'});
+    return response.end(JSON.stringify({status: 'OK'}));
 
-  requestInfo.logs.push({
-    method: request.method,
-    url: request.url,
+  })
+
+
+}).listen(port, () => {
+  console.log('Server is listening on 3000...');
+});
+
+
+async function requestAll() {
+  return await readFileAsync(path.join(__dirname, 'requests.json'), 'utf-8');
+}
+
+async function requestsManager(req) {
+  const data = await readFileAsync(path.join(__dirname, 'requests.json'), 'utf-8');
+
+  const dataParsed = JSON.parse(data);
+  dataParsed.logs.push({
+    method: req.method,
+    url: req.url,
     time: new Date().getTime()
   });
 
-  fs.writeFile(path.join(__dirname, 'requests.json'), JSON.stringify(requestInfo, null, 4), () => {
-    console.log('Json file was updated!')
-  });
+  return await writeFileAsync(path.join(__dirname, 'requests.json'), JSON.stringify(dataParsed, null, 4), 'utf-8');
 
-  response.writeHead(200, {'Content-type': 'text/json'});
-  response.end(JSON.stringify({status: 'OK'}));
-
-}).listen(3000, () => {
-  console.log('Server is listening on 3000...');
-});
+}
